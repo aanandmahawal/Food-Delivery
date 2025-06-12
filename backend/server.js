2a065f628cf08503cca30c4a536c1a9aaa7e9277
aanandmahawal <151204835+aanandmahawal@users.noreply.github.com>
2025-06-13 01:04:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ const port=process.env.PORT || 4000;
 
 //middleware
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin : "https://food-delivery-frontend-5lem.onrender.com",
+    credentials : true
+}))
 
 //db connection
 connectDB();
